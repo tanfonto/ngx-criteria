b407b2fdaa2129f460365fd77382971137936f9d
Whapky <sieradzpf@gmail.com>
2018-06-17 14:24:39 +0200

    cleanup

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.All = Symbol('All');
-__export(require("./spec-builder"));
